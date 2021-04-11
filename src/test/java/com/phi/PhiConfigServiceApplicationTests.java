4508a1a87a1bdcaadcb43f12284abd3e999971ed
thanhvt <thanhvt@vissoft.vn>
2021-04-12 00:13:04 +0700

    init commit

---
+++ src/test/java/com/phi/PhiConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhiConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
