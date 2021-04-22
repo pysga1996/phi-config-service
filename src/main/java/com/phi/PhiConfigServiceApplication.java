8ce93b5b201d569e5c59e888686613b194f70b69
thanhvt <thanhvt@vissoft.vn>
2021-04-23 02:36:04 +0700

    add Dockerfile
    add Dockerfile

--- src/main/java/com/phi/PhiConfigServiceApplication.java
+++ src/main/java/com/phi/PhiConfigServiceApplication.java
@@ -1,13 +1,15 @@
 package com.phi;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@Log4j2
 @EnableConfigServer
 @EnableEurekaClient
+@SpringBootApplication(scanBasePackages = {"com.phi"})
 public class PhiConfigServiceApplication {
 
     public static void main(String[] args) {
